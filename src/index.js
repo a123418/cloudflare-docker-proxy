f17ae488f5fad15f0cf17558d0ba5b0a9319bcca
a123418 <tian123418@gmail.com>
2024-10-07 20:17:37 +0800

    Update index.js
    改为自己的URL

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "registry.docker.jing7.top": dockerHub,
+  "quay.docker.jing7.top": "https://quay.io",
+  "gcr.docker.jing7.top": "https://gcr.io",
+  "k8s-gcr.docker.jing7.top": "https://k8s.gcr.io",
+  "k8s.docker.jing7.top": "https://registry.k8s.io",
+  "ghcr.docker.jing7.top": "https://ghcr.io",
+  "cloudsmith.docker.jing7.top": "https://docker.cloudsmith.io",
+  "ecr.docker.jing7.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.docker.jing7.top": dockerHub,
 };
 
 function routeByHosts(host) {
